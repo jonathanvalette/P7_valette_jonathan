f40ddc720a96afdee1a04c032e502be8481a466e
joanthanvalette <jonathanvalette1204@gmail.com>
2022-03-02 12:18:50 +0100

    update

--- back/constraints/Password.js
+++ back/constraints/Password.js
@@ -5,6 +5,5 @@ const passwordSchema = new passwordValidator();
 passwordSchema
 .is().min(6)                                                                  // La longueur minimale du mdp est de 6 caractères
 .is().max(24)                                                                 // La longueur maximale du mdp est de 24 caractères
-      // Exclusion de mdp trop simples
 
 module.exports = passwordSchema;
